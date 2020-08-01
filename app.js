263567b3d44fadfceb2111618144bbfc6df8a68f
Denys Matsenko <idanchik47@gmail.com>
2020-08-01 21:35:07 +0300

    added progress bar

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const paperBtn = document.getElementById("paper-btn");
 const scissorsBtn = document.getElementById("scissors-btn");
 const result = document.getElementById("result");
 const opponent = document.getElementById("opponent-text");
+const progressBar = document.querySelectorAll("li");
 
 const ROCK = "ROCK";
 const PAPER = "PAPER";
@@ -13,9 +14,31 @@ const choices = [ROCK, PAPER, SCISSORS];
 
 let userChoice;
 let opponentChoice;
+let counter = 0;
+let reset = false;
 
 function showInfo() {
     alert("It's not done yet!");
+}
+
+function isFinished(){
+    if(counter === 10){
+        counter = 0;
+        alert("Round has finished!");
+        result.textContent = "Play again!";
+        result.className = "";
+        reset = true;
+    }
+}
+
+function isReset(){
+    if(reset){
+        for (let index = 0; index < progressBar.length; index++) {
+            progressBar[index].textContent = "";
+            progressBar[index].className = "";
+        }
+        reset = false;
+    }
 }
 
 function getResult() {
@@ -26,18 +49,32 @@ function getResult() {
         (userChoice == SCISSORS && opponentChoice == PAPER) ||
         (userChoice == PAPER && opponentChoice == ROCK)
     ) {
+        isReset();
+        const index = counter++;
         opponent.textContent = opponentChoice;
         result.className = "result-win";
         result.textContent = "You've won!";
+        progressBar[index].className = "result-win";
+        progressBar[index].textContent = "W";
+        isFinished();
     } else if (userChoice == opponentChoice) {
+        isReset();
+        const index = counter++;
         opponent.textContent = opponentChoice;
         result.className = "result-draw";
         result.textContent = "It's a draw!";
+        progressBar[index].className = "result-draw";
+        progressBar[index].textContent = "D";
+        isFinished();
     } else {
+        isReset();
+        const index = counter++;
         opponent.textContent = opponentChoice;
         result.className = "result-loss";
         result.textContent = "You've lost.";
-
+        progressBar[index].className = "result-loss";
+        progressBar[index].textContent = "L";
+        isFinished();
     }
 }
 
