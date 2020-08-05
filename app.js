6d113b073bccbed936033c08e82c7bd4cca611a2
Denys Matsenko <idanchik47@gmail.com>
2020-08-05 14:34:22 +0300

    added results menu when round is finished

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const progressBar = document.querySelectorAll("li");
 const howToPlayModal = document.querySelector(".how-to-play-modal");
 const backdrop = document.getElementById("backdrop");
 const closeModalBtn = document.getElementById("close-modal-btn");
+const conclusionModal = document.querySelector(".conclusion-modal");
+
 
 const ROCK = "ROCK";
 const PAPER = "PAPER";
@@ -19,13 +21,24 @@ let userChoice;
 let opponentChoice;
 let counter = 0;
 let reset = false;
+let counterWins = 0;
+let counterDraws = 0;
+let counterLosses = 0;
 
-function toggleModal(){
-    howToPlayModal.classList.toggle("visible");
+function showModal(){
+    howToPlayModal.classList.add("visible");
 }
 
-function toggleBackdrop(){
-    backdrop.classList.toggle("visible");
+function closeModal(){
+    howToPlayModal.classList.remove("visible");
+}
+
+function showBackdrop(){
+    backdrop.classList.add("visible");
+}
+
+function closeBackdrop(){
+    backdrop.classList.remove("visible");
 }
 
 function isFinished(){
@@ -34,10 +47,37 @@ function isFinished(){
         result.textContent = "Play again!";
         result.className = "";
         reset = true;
+        showConclusionModal();
     }
 }
 
+function closeConclusionModal(){
+    conclusionModal.classList.remove("visible");
+    closeBackdrop();
+    isReset();
+}
+
+function showConclusionModal(){
+    conclusionModal.classList.add("visible");
+    showBackdrop();
+    const closeModalBtn = document.getElementById("conlusion-modal-close");
+    closeModalBtn.addEventListener("click", closeConclusionModal);
+    
+    const numWins = document.getElementById("conclusion-wins");
+    const numLosses = document.getElementById("conclusion-losses");
+    const numDraws = document.getElementById("conclusion-draws");
+
+    numWins.textContent = counterWins;
+    numWins.className = "text-win";
+    numDraws.textContent = counterDraws;
+    numLosses.textContent = counterLosses;
+    numLosses.className = "text-loss";
+}
+
 function isReset(){
+    counterLosses = 0;
+    counterWins = 0;
+    counterDraws = 0;
     if(reset){
         for (let index = 0; index < progressBar.length; index++) {
             progressBar[index].textContent = "";
@@ -55,31 +95,31 @@ function getResult() {
         (userChoice == SCISSORS && opponentChoice == PAPER) ||
         (userChoice == PAPER && opponentChoice == ROCK)
     ) {
-        isReset();
         const index = counter++;
         opponent.textContent = opponentChoice;
         result.className = "result-win";
         result.textContent = "You've won!";
         progressBar[index].className = "result-win";
         progressBar[index].textContent = "W";
+        counterWins++;
         isFinished();
     } else if (userChoice == opponentChoice) {
-        isReset();
         const index = counter++;
         opponent.textContent = opponentChoice;
         result.className = "result-draw";
         result.textContent = "It's a draw!";
         progressBar[index].className = "result-draw";
         progressBar[index].textContent = "D";
+        counterDraws++;
         isFinished();
     } else {
-        isReset();
         const index = counter++;
         opponent.textContent = opponentChoice;
         result.className = "result-loss";
         result.textContent = "You've lost.";
         progressBar[index].className = "result-loss";
         progressBar[index].textContent = "L";
+        counterLosses++;
         isFinished();
     }
 }
@@ -100,8 +140,8 @@ function isScissors() {
 }
 
 howToPlayBtn.addEventListener("click", () =>{
-    toggleModal();
-    toggleBackdrop();
+    showModal();
+    showBackdrop();
 });
 rockBtn.addEventListener("click", () =>{
     userChoice = ROCK;
@@ -116,10 +156,11 @@ scissorsBtn.addEventListener("click", () =>{
     getResult();
 });
 backdrop.addEventListener("click", ()=>{
-    toggleModal();
-    toggleBackdrop();
+    closeModal();
+    closeBackdrop();
+    closeConclusionModal();
 });
 closeModalBtn.addEventListener("click", ()=>{
-    toggleModal();
-    toggleBackdrop();
+    closeModal();
+    closeBackdrop();
 });
